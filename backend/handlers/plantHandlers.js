1eff73b3121a87c754f494b7db8a62f2a295ecc3
Victoria Peart <v-peart@outlook.com>
2021-04-11 17:22:41 -0700

    request plant data from mongodb

--- backend/handlers/plantHandlers.js
+++ backend/handlers/plantHandlers.js
@@ -1,7 +1,27 @@
-let plants = require("../data/plants.json");
+const { MongoClient } = require("mongodb");
+// const assert = require("assert");
+require("dotenv").config();
+const { MONGO_URI } = process.env;
 
-const getPlants = (req, res) => {
-  res.status(200).json({ status: 200, data: plants });
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+// let plants = require("../data/plants.json");
+
+const getPlants = async (req, res) => {
+  const client = await MongoClient(MONGO_URI, options);
+  await client.connect();
+  const db = client.db("plantgeekdb");
+  const plants = await db.collection("plants").find().toArray();
+
+  if (plants) {
+    res.status(200).json({ status: 200, data: plants });
+  } else {
+    res.status(404).json({ status: 404, message: "Oops, nothing here!" });
+  }
+  client.close();
 };
 
 module.exports = { getPlants };
