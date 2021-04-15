3e87decdbd3c87b615ecab8148c1fc71bcab07a2
Victoria Peart <v-peart@outlook.com>
2021-04-14 17:59:29 -0700

    create basic user profile

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { usersArray } from "../reducers/userReducer";
+import styled from "styled-components";
+import background from "../assets/monstera-bg.jpg";
+import placeholder from "../assets/avatar-placeholder.png";
 
 export const Profile = () => {
   const users = useSelector(usersArray);
@@ -12,5 +15,52 @@ export const Profile = () => {
     setUser(users.find((user) => user._id === id));
   }, [users, user, id]);
 
-  return <div>{user && <>{user.name}'s profile</>}</div>;
+  return (
+    <Wrapper>
+      <Banner />
+      {user && (
+        <>
+          <Avatar src={user.avatar ? user.avatar : placeholder} alt="" />
+          <Name>{user.name}</Name>
+          {/* <Username>{user.username}</Username> */}
+          {user.collection && <Collection />}
+          {user.favorites && <Favorites />}
+          {user.wishlist && <Wishlist />}
+        </>
+      )}
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Banner = styled.div`
+  background: url(${background}) center center / cover;
+  height: 120px;
+  width: 100%;
+`;
+
+const Avatar = styled.img`
+  height: 200px;
+  width: 200px;
+  border-radius: 50%;
+  position: relative;
+  top: -70px;
+`;
+
+const Name = styled.h1`
+  position: relative;
+  top: -60px;
+`;
+
+const Username = styled.p``;
+
+const Collection = styled.ul``;
+
+const Favorites = styled.ul``;
+
+const Wishlist = styled.ul``;
