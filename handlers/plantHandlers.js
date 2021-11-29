0fbd144dee23c3d7998b4160694555ae6edad41f
Victoria Peart <73324816+v-prt@users.noreply.github.com>
2021-11-28 17:42:36 -0800

    update getting MONGO_URI from env

--- handlers/plantHandlers.js
+++ handlers/plantHandlers.js
@@ -1,71 +1,71 @@
-const { MongoClient, ObjectId } = require("mongodb");
+const { MongoClient, ObjectId } = require('mongodb')
 // const assert = require("assert");
-require("dotenv").config();
-const { MONGO_URI } = process.env;
+require('dotenv').config()
+const MONGO_URI = process.env.MONGO_URI
 
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-};
+}
 
 // GETS ALL PLANTS IN DATABASE
 const getPlants = async (req, res) => {
-  const client = await MongoClient(MONGO_URI, options);
-  await client.connect();
-  const db = client.db("plantgeekdb");
-  const plants = await db.collection("plants").find().toArray();
+  const client = await MongoClient(MONGO_URI, options)
+  await client.connect()
+  const db = client.db('plantgeekdb')
+  const plants = await db.collection('plants').find().toArray()
   if (plants) {
-    res.status(200).json({ status: 200, data: plants });
+    res.status(200).json({ status: 200, data: plants })
   } else {
-    res.status(404).json({ status: 404, message: "Oops, nothing here!" });
+    res.status(404).json({ status: 404, message: 'Oops, nothing here!' })
   }
-  client.close();
-};
+  client.close()
+}
 
 // GETS PLANT BY ID FROM DATABASE
 const getPlant = async (req, res) => {
-  const client = await MongoClient(MONGO_URI, options);
-  const _id = req.params._id;
-  await client.connect();
-  const db = client.db("plantgeekdb");
-  const plant = await db.collection("plants").findOne({ _id: ObjectId(_id) });
+  const client = await MongoClient(MONGO_URI, options)
+  const _id = req.params._id
+  await client.connect()
+  const db = client.db('plantgeekdb')
+  const plant = await db.collection('plants').findOne({ _id: ObjectId(_id) })
   if (plant) {
-    res.status(200).json({ status: 200, data: plant });
+    res.status(200).json({ status: 200, data: plant })
   } else {
-    res.status(404).json({ status: 404, message: "Oops, nothing here!" });
+    res.status(404).json({ status: 404, message: 'Oops, nothing here!' })
   }
-  client.close();
-};
+  client.close()
+}
 
 // GETS PLANT BY ID & POSTS NEW COMMENT
 const postComment = async (req, res) => {
-  const client = await MongoClient(MONGO_URI, options);
-  const _id = req.params._id;
+  const client = await MongoClient(MONGO_URI, options)
+  const _id = req.params._id
   try {
-    await client.connect();
-    const db = client.db("plantgeekdb");
-    const plants = db.collection("plants");
-    const filter = { _id: ObjectId(_id) };
-    let update = undefined;
+    await client.connect()
+    const db = client.db('plantgeekdb')
+    const plants = db.collection('plants')
+    const filter = { _id: ObjectId(_id) }
+    let update = undefined
     if (req.body.comments) {
       update = {
         $push: {
           comments: req.body.comments,
         },
-      };
+      }
     }
-    const result = await plants.updateOne(filter, update);
+    const result = await plants.updateOne(filter, update)
     res.status(200).json({
       status: 200,
       message: `${result.matchedCount} plant(s) matched the filter, updated ${result.modifiedCount} plant(s)`,
-    });
+    })
     console.log(
       `${result.matchedCount} plant(s) matched the filter, updated ${result.modifiedCount} plant(s)`
-    );
+    )
   } catch (err) {
-    res.status(404).json({ status: 404, message: err.message });
+    res.status(404).json({ status: 404, message: err.message })
   }
-  client.close();
-};
+  client.close()
+}
 
-module.exports = { getPlants, getPlant, postComment };
+module.exports = { getPlants, getPlant, postComment }
--- handlers/userHandlers.js
+++ handlers/userHandlers.js
@@ -1,23 +1,23 @@
-const { MongoClient, ObjectId } = require("mongodb");
-const assert = require("assert");
-require("dotenv").config();
-const { MONGO_URI } = process.env;
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const { MongoClient, ObjectId } = require('mongodb')
+const assert = require('assert')
+require('dotenv').config()
+const MONGO_URI = process.env.MONGO_URI
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-};
+}
 
 // (CREATE/POST) ADD A NEW USER TO DATABASE
 const createUser = async (req, res) => {
-  const client = await MongoClient(MONGO_URI, options);
+  const client = await MongoClient(MONGO_URI, options)
   try {
-    await client.connect();
-    const db = client.db("plantgeekdb");
-    const hashedPwd = await bcrypt.hash(req.body.password, saltRounds);
-    const user = await db.collection("users").insertOne({
+    await client.connect()
+    const db = client.db('plantgeekdb')
+    const hashedPwd = await bcrypt.hash(req.body.password, saltRounds)
+    const user = await db.collection('users').insertOne({
       username: req.body.username,
       password: hashedPwd,
       joined: new Date(),
@@ -25,164 +25,162 @@ const createUser = async (req, res) => {
       collection: [],
       favorites: [],
       wishlist: [],
-    });
-    assert.strictEqual(1, user.insertedCount);
-    res.status(201).json({ status: 201, data: user });
+    })
+    assert.strictEqual(1, user.insertedCount)
+    res.status(201).json({ status: 201, data: user })
   } catch (err) {
-    res.status(500).json({ status: 500, data: req.body, message: err.message });
-    console.log(err.stack);
+    res.status(500).json({ status: 500, data: req.body, message: err.message })
+    console.log(err.stack)
   }
-  client.close();
-};
+  client.close()
+}
 
 // (READ/POST) AUTHENTICATE USER
 const authenticateUser = async (req, res) => {
-  const client = await MongoClient(MONGO_URI, options);
+  const client = await MongoClient(MONGO_URI, options)
   try {
-    await client.connect();
-    const db = client.db("plantgeekdb");
-    const user = await db
-      .collection("users")
-      .findOne({ username: req.body.username });
+    await client.connect()
+    const db = client.db('plantgeekdb')
+    const user = await db.collection('users').findOne({ username: req.body.username })
     if (user) {
-      const cmp = await bcrypt.compare(req.body.password, user.password);
+      const cmp = await bcrypt.compare(req.body.password, user.password)
       if (cmp) {
-        res.status(200).json({ status: 200, data: user });
+        res.status(200).json({ status: 200, data: user })
       } else {
-        res.status(403).json({ status: 403, message: "Incorrect password" });
+        res.status(403).json({ status: 403, message: 'Incorrect password' })
       }
     } else {
-      res.status(401).json({ status: 401, message: "Incorrect username" });
+      res.status(401).json({ status: 401, message: 'Incorrect username' })
     }
   } catch (err) {
-    console.log(err);
-    res.status(500).send("Internal server error");
+    console.log(err)
+    res.status(500).send('Internal server error')
   }
-};
+}
 
 // (READ/GET) GETS ALL USERS IN DATABASE
 const getUsers = async (req, res) => {
-  const client = await MongoClient(MONGO_URI, options);
-  await client.connect();
-  const db = client.db("plantgeekdb");
-  const users = await db.collection("users").find().toArray();
+  const client = await MongoClient(MONGO_URI, options)
+  await client.connect()
+  const db = client.db('plantgeekdb')
+  const users = await db.collection('users').find().toArray()
   if (users) {
-    res.status(200).json({ status: 200, data: users });
+    res.status(200).json({ status: 200, data: users })
   } else {
-    res.status(404).json({ status: 404, message: "Oops, nothing here!" });
+    res.status(404).json({ status: 404, message: 'Oops, nothing here!' })
   }
-  client.close();
-};
+  client.close()
+}
 
 // (UPDATE/PUT) ADD A PLANT, FRIEND, OR IMAGE TO USER'S PROFILE
 const addToUser = async (req, res) => {
-  const client = await MongoClient(MONGO_URI, options);
-  const username = req.params.username;
+  const client = await MongoClient(MONGO_URI, options)
+  const username = req.params.username
   try {
-    await client.connect();
-    const db = client.db("plantgeekdb");
-    const users = db.collection("users");
-    const filter = { username: username };
-    let update = undefined;
+    await client.connect()
+    const db = client.db('plantgeekdb')
+    const users = db.collection('users')
+    const filter = { username: username }
+    let update = undefined
     if (req.body.collection) {
       update = {
         $push: {
           collection: req.body.collection,
         },
-      };
+      }
     } else if (req.body.favorites) {
       update = {
         $push: {
           favorites: req.body.favorites,
         },
-      };
+      }
     } else if (req.body.wishlist) {
       update = {
         $push: {
           wishlist: req.body.wishlist,
         },
-      };
+      }
     } else if (req.body.friends) {
       update = {
         $push: {
           friends: req.body.friends,
         },
-      };
+      }
     } else if (req.body.image) {
       update = {
         $push: {
           image: req.body.image,
         },
-      };
+      }
     }
-    const result = await users.updateOne(filter, update);
+    const result = await users.updateOne(filter, update)
     res.status(200).json({
       status: 200,
       message: `${result.matchedCount} user(s) matched the filter, updated ${result.modifiedCount} user(s)`,
-    });
+    })
     console.log(
       `${result.matchedCount} user(s) matched the filter, updated ${result.modifiedCount} user(s)`
-    );
+    )
   } catch (err) {
-    res.status(404).json({ status: 404, message: err.message });
+    res.status(404).json({ status: 404, message: err.message })
   }
-  client.close();
-};
+  client.close()
+}
 
 // (UPDATE/PUT) REMOVE A PLANT, FRIEND, OR IMAGE FROM USER'S PROFILE
 const removeFromUser = async (req, res) => {
-  const client = await MongoClient(MONGO_URI, options);
-  const username = req.params.username;
+  const client = await MongoClient(MONGO_URI, options)
+  const username = req.params.username
   try {
-    await client.connect();
-    const db = client.db("plantgeekdb");
-    const users = db.collection("users");
-    const filter = { username: username };
-    let update = undefined;
+    await client.connect()
+    const db = client.db('plantgeekdb')
+    const users = db.collection('users')
+    const filter = { username: username }
+    let update = undefined
     if (req.body.collection) {
       update = {
         $pull: {
           collection: req.body.collection,
         },
-      };
+      }
     } else if (req.body.favorites) {
       update = {
         $pull: {
           favorites: req.body.favorites,
         },
-      };
+      }
     } else if (req.body.wishlist) {
       update = {
         $pull: {
           wishlist: req.body.wishlist,
         },
-      };
+      }
     } else if (req.body.friends) {
       update = {
         $pull: {
           friends: req.body.friends,
         },
-      };
+      }
     } else if (req.body.image) {
       update = {
         $pull: {
           image: req.body.image,
         },
-      };
+      }
     }
-    const result = await users.updateOne(filter, update);
+    const result = await users.updateOne(filter, update)
     res.status(200).json({
       status: 200,
       message: `${result.matchedCount} user(s) matched the filter, updated ${result.modifiedCount} user(s)`,
-    });
+    })
     console.log(
       `${result.matchedCount} user(s) matched the filter, updated ${result.modifiedCount} user(s)`
-    );
+    )
   } catch (err) {
-    res.status(404).json({ status: 404, message: err.message });
+    res.status(404).json({ status: 404, message: err.message })
   }
-  client.close();
-};
+  client.close()
+}
 
 // TODO: (DELETE) REMOVE A USER FROM DB
 
@@ -192,4 +190,4 @@ module.exports = {
   getUsers,
   addToUser,
   removeFromUser,
-};
+}
