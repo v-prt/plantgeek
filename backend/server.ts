15a0fa4dac1d17b63b47270a5d9d30f9fedb22e3
Victoria Peart <73324816+v-prt@users.noreply.github.com>
2022-11-11 17:11:11 -0800

    Update server.ts

--- backend/server.ts
+++ backend/server.ts
@@ -98,11 +98,11 @@ app
   .put(`${API_URL}/suggestions/:id`, updateSuggestion)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'frontend', 'build')))
+  app.use(express.static(path.join(__dirname, '../frontend', 'build')))
 }
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
+  res.sendFile(path.join(__dirname, '../frontend', 'build', 'index.html'))
 })
 
 app.listen(PORT, () => console.info(`Listening on port ${PORT}`))
