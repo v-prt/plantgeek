5181cfe5c8e9b523309fc5ff15ef6b86496951e0
j-prt <peart.l.joe@gmail.com>
2024-03-03 17:18:05 -0800

    update vite config to be compatible with antd and less

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vitePluginImp from 'vite-plugin-imp'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    vitePluginImp({
+      libList: [
+        {
+          libName: 'antd',
+          style: name => `antd/es/${name}/style`,
+        },
+      ],
+    }),
+  ],
   css: {
     preprocessorOptions: {
       less: {
@@ -14,5 +25,8 @@ export default defineConfig({
         javascriptEnabled: true,
       },
     },
+  },
+  resolve: {
+    alias: [{ find: /^~/, replacement: '' }],
   },
 })
