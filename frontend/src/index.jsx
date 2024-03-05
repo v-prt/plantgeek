f04124baa4b31f4a59c2f35741a5c5f2497c80d6
j-prt <peart.l.joe@gmail.com>
2024-03-04 17:24:48 -0800

    update dev/prod deps, update to react 18

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import { App } from './App'
 import { Provider } from 'react-redux'
 import configureStore from './store'
@@ -24,7 +24,7 @@ const queryClient = new QueryClient({
 
 const store = configureStore()
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <QueryClientProvider client={queryClient}>
     <Provider store={store}>
       <UserProvider>
@@ -34,6 +34,5 @@ ReactDOM.render(
       </UserProvider>
     </Provider>
     <ReactQueryDevtools initialIsOpen={false} position='bottom-left' />
-  </QueryClientProvider>,
-  document.getElementById('root')
+  </QueryClientProvider>
 )
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -35,17 +35,6 @@ export default ({ mode }) => {
           target: 'http://localhost:4000',
           changeOrigin: true,
           secure: false,
-          configure: (proxy, _options) => {
-            proxy.on('error', (err, _req, _res) => {
-              console.log('proxy error', err)
-            })
-            proxy.on('proxyReq', (proxyReq, req, _res) => {
-              console.log('Sending Request to the Target:', req.method, req.url)
-            })
-            proxy.on('proxyRes', (proxyRes, req, _res) => {
-              console.log('Received Response from the Target:', proxyRes.statusCode, req.url)
-            })
-          },
         },
       },
     },
