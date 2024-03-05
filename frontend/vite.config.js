7391ac5f0b98f1884f6b6e770632c8ea8ca6cfe4
j-prt <peart.l.joe@gmail.com>
2024-03-04 21:12:35 -0800

    update packages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import vitePluginImp from 'vite-plugin-imp'
 
 // https://vitejs.dev/config/
 
@@ -18,17 +17,7 @@ export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
   return defineConfig({
-    plugins: [
-      react(),
-      vitePluginImp({
-        libList: [
-          {
-            libName: 'antd',
-            style: name => `antd/es/${name}/style`,
-          },
-        ],
-      }),
-    ],
+    plugins: [react()],
     server: {
       proxy: {
         [env.VITE_REACT_APP_API_URL]: {
