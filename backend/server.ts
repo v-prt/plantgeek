3a4f94be73dbe4bab284fbb6bc48366fd6c66abc
j-prt <peart.l.joe@gmail.com>
2024-03-05 16:45:50 -0800

    enable https

--- backend/server.ts
+++ backend/server.ts
@@ -4,6 +4,8 @@ import express, { Express } from 'express'
 import mongoose from 'mongoose'
 import morgan from 'morgan' // logs request on the terminal (example: Get /users 100ms 200)
 import path from 'path'
+import fs from 'fs'
+import https from 'https'
 import routes from './Routes'
 
 import sourceMapSupport from 'source-map-support'
@@ -35,9 +37,14 @@ app.get('*', (req, res) => {
 
 // app.listen(PORT, () => console.info(`Listening on port ${PORT}`))
 
+const key = fs.readFileSync(path.join(__dirname, 'cf_key.pem'))
+const cert = fs.readFileSync(path.join(__dirname, 'cf_cert.pem'))
+
+const server = https.createServer({ key, cert }, app)
+
 mongoose
   .connect(uri)
-  .then(() => app.listen(PORT, () => console.info(`Listening on port ${PORT}`)))
+  .then(() => server.listen(PORT, () => console.info(`Listening on port ${PORT}`)))
   .catch(error => console.error(error))
 
 // Mongoose.connection.close now returns a promise, instead of accepting a callback arg
